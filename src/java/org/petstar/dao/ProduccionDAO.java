b26b6aaa00612574ba88d24ba2b7f84f4bbbb281
Alfredo Neri <alfredo.neri@gmail.com>
2018-05-04 18:18:19 -0500

    Service de Produccion
    Se crea servicio de Produccion

---
+++ src/java/org/petstar/dao/ProduccionDAO.java
@@ -0,0 +1,42 @@
+package org.petstar.dao;
+
+import java.util.List;
+import javax.sql.DataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.petstar.configurations.PoolDataSource;
+import org.petstar.dto.ProduccionDTO;
+
+/**
+ *
+ * @author Tech-Pro
+ */
+public class ProduccionDAO {
+    
+    public List<ProduccionDTO> getProduccionByPeriodo(int mes, int anio) throws Exception{
+        DataSource ds = PoolDataSource.getDataSource();
+        QueryRunner qr = new QueryRunner(ds);
+        StringBuilder sql = new StringBuilder();
+        
+        sql.append("EXEC sp_selectPetProduccionByAnioMes ?, ?");
+        Object[] params = { mes, anio };
+        
+        ResultSetHandler rsh = new BeanListHandler(ProduccionDTO.class);
+        List<ProduccionDTO> list = (List<ProduccionDTO>) qr.query(sql.toString(), rsh, params);
+        return list;
+    }
+    
+    public List<ProduccionDTO> getProduccionByPeriodoAndLinea(int mes, int anio, int idLinea) throws Exception{
+        DataSource ds = PoolDataSource.getDataSource();
+        QueryRunner qr = new QueryRunner(ds);
+        StringBuilder sql = new StringBuilder();
+        
+        sql.append("EXEC sp_selectProduccionByPeriodo ?, ?, ?");
+        Object[] params = { mes, anio, idLinea };
+        
+        ResultSetHandler rsh = new BeanListHandler(ProduccionDTO.class);
+        List<ProduccionDTO> list = (List<ProduccionDTO>) qr.query(sql.toString(), rsh, params);
+        return list;
+    }
+}
---
+++ src/java/org/petstar/dto/ProduccionDTO.java
@@ -0,0 +1,137 @@
+package org.petstar.dto;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ *
+ * @author Tech-Pro
+ */
+public class ProduccionDTO {
+    private int id_produccion;
+    private BigDecimal valor;
+    private int id_meta;
+    private Date dia;
+    private String diaString;
+    private int id_grupo;
+    private String valor_grupo;
+    private int id_turno;
+    private String valor_turno;
+    private int id_linea;
+    private String valor_linea;
+    private int id_producto;
+    private String valor_producto;
+    private int liberado;
+
+    public int getId_produccion() {
+        return id_produccion;
+    }
+
+    public void setId_produccion(int id_produccion) {
+        this.id_produccion = id_produccion;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public int getId_meta() {
+        return id_meta;
+    }
+
+    public void setId_meta(int id_meta) {
+        this.id_meta = id_meta;
+    }
+
+    public Date getDia() {
+        return dia;
+    }
+
+    public void setDia(Date dia) {
+        this.dia = dia;
+    }
+
+    public String getDiaString() {
+        return diaString;
+    }
+
+    public void setDiaString(String diaString) {
+        this.diaString = diaString;
+    }
+
+    public int getId_grupo() {
+        return id_grupo;
+    }
+
+    public void setId_grupo(int id_grupo) {
+        this.id_grupo = id_grupo;
+    }
+
+    public String getValor_grupo() {
+        return valor_grupo;
+    }
+
+    public void setValor_grupo(String valor_grupo) {
+        this.valor_grupo = valor_grupo;
+    }
+
+    public int getId_turno() {
+        return id_turno;
+    }
+
+    public void setId_turno(int id_turno) {
+        this.id_turno = id_turno;
+    }
+
+    public String getValor_turno() {
+        return valor_turno;
+    }
+
+    public void setValor_turno(String valor_turno) {
+        this.valor_turno = valor_turno;
+    }
+
+    public int getId_linea() {
+        return id_linea;
+    }
+
+    public void setId_linea(int id_linea) {
+        this.id_linea = id_linea;
+    }
+
+    public String getValor_linea() {
+        return valor_linea;
+    }
+
+    public void setValor_linea(String valor_linea) {
+        this.valor_linea = valor_linea;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getValor_producto() {
+        return valor_producto;
+    }
+
+    public void setValor_producto(String valor_producto) {
+        this.valor_producto = valor_producto;
+    }
+
+    public int getLiberado() {
+        return liberado;
+    }
+
+    public void setLiberado(int liberado) {
+        this.liberado = liberado;
+    }
+}
